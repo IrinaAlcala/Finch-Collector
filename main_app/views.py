e8c44c1c4178c1262fc6c5b980850841e3f46636
IrinaAlcala <irinaalcala@Jasons-MBP.fios-router.home>
2020-04-29 16:01:11 -0700

    added CRUD

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 # Create your views here.
 def home(request):
@@ -8,10 +10,23 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def finches_index(request):
-    finches = Finch.objects.all()
-    return render(request, 'finches/index.html', {'finches': finches })
+class FinchList(ListView):
+    model = Finch
 
-def finches_detail(request, finch_id):
-    finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', { 'finch': finch })
+def get_queryset(self):
+    return Finch.objects.all()
+    
+class FinchDetail(DetailView):
+    model = Finch
+
+class FinchCreate(CreateView):
+     model = Finch
+     fields = '__all__' 
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
