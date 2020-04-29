713791d808481ccfee01f39647a7a8a27dc9fb01
IrinaAlcala <irinaalcala@Jasons-MBP.fios-router.home>
2020-04-28 19:14:10 -0700

    added models

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
-
+from .models import Finch
 # Create your views here.
-from django.http import HttpResponse
-
-class Finch:
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
-finches = [
-  Finch('Tenor', 'house finch', 'great for households', 1),
-  Finch('Squeak', 'european finch', 'likes cold weather', 2),
-  Finch('Queen', 'purple finch', 'beautiful purple color', 3)
-]
-# Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello</h1>')
+  return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
